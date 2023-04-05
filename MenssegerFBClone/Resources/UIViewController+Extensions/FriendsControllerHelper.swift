bdd7e69671b89db552c2e76f2363bf89eda4e661
kaueludovico <kaueludovico@outlook.com>
2023-04-04 21:10:18 -0300

    Inserindo novas regras de negocio a CollectionView referente aos dados

--- MenssegerFBClone/Resources/UIViewController+Extensions/FriendsControllerHelper.swift
+++ MenssegerFBClone/Resources/UIViewController+Extensions/FriendsControllerHelper.swift
@@ -10,7 +10,34 @@ import CoreData
 
 extension FriendsViewController {
     
+    func clearData() {
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        
+        if let context = delegate?.persistentContainer.viewContext {
+            
+            do {
+                
+                let entityNames = ["Friend","Message"]
+                
+                for entityName in entityNames {
+                    let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+                    let objects = try(context.fetch(fetchRequest)) as? [NSManagedObject]
+                    for object in objects! {
+                        context.delete(object)
+                    }
+                }
+                try(context.save())
+                
+            } catch let err{
+                print(err)
+            }
+
+        }
+    }
+    
     func setupData() {
+        
+        clearData()
         
         let delegate = UIApplication.shared.delegate as? AppDelegate
         
@@ -32,8 +59,29 @@ extension FriendsViewController {
             messageSteve.friend = steve
             messageSteve.text = "Apple created great iOS devices for the world..."
             messageSteve.date = Date()
+            do {
+                try(context.save())
+            }catch let err {
+                print(err)
+            }
             
-            messages = [message, messageSteve]
+//            messages = [message, messageSteve]
+        }
+        
+        loadData()
+        
+    }
+    func loadData() {
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        
+        if let context = delegate?.persistentContainer.viewContext {
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Message")
+            do {
+                messages = try (context.fetch(fetchRequest)) as? [Message]
+            } catch let err{
+                print(err)
+            }
         }
     }
 }
